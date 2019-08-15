5f2bc87d3994b63bec03396a3743ff641dac0d11
Scott Marion <smarion@affordabletool.com>
2019-08-15 13:48:02 -0400

    chapter Getting Real with APIs completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const largeColumn = {
   width: '40%',
@@ -30,95 +21,149 @@ const smallColumn = {
   width: '10%',
 };
 
-/*
-function isSearched(searchTerm) {
-  return function (item) {
-    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-  }
-}
-*/
-// same as above with refactoring with arrow function to make more concise
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
 class App extends Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      list,
-      searchTerm: '',
-    }
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+      error: null,
+    };
     
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+  
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+  
+  setSearchTopStories(result) {
+
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+      
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];  
+      
+    this.setState({ 
+        results: {
+          ...results,
+          [searchKey]: { hits: updatedHits, page }
+        }
+    });    
+  }
+  
+  fetchSearchTopStories(searchTerm, page = 0) {
+      const url=`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`;
+      console.log('fetch url', url);
+      axios(url)
+      .then(result => this.setSearchTopStories(result.data))
+      .catch(error => this.setState({ error }));    
+  }
+  
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
   }
   
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });    
   }
   
-  onDismiss(id) {
-    /* first try
-    const updatedList = this.state.list.filter(function isNotId(item) {
-      return item.objectID !== id;    
-    });
-    */
-    
-    /* 2nd try - extract the function from the statement
-    function isNotId(item) {
-        return item.objectID !== id;
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
     }
-    
-    const updatedList = this.state.list.filter(isNotId);
-    */
-    
-    /* 3rd try - use arrow function instead of regular function to filter more efficiently */
+    event.preventDefault();
+  }
+  
+  onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];      
+      
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    
-    
-    /* 4th try - inline it all - although it may be less readable
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    */
-    
-    this.setState({ list: updatedList });
+    const updatedHits = hits.filter(isNotId);
+    this.setState({
+      results: { 
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
   }
     
   render() {
-      const { searchTerm, list } = this.state;
-      return (
+    const { searchTerm, results, searchKey, error } = this.state;
+    const page = (
+      results && 
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+    return (
       <div className="page">
         <div className="interactions">
           <Search 
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Search
           </Search>
         </div>
-        <Table 
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { error 
+          ? <div class="interactions">
+            <p>Something went wrong!</p>
+          </div>
+          : <Table 
+            list={list}
+            onDismiss={this.onDismiss}
+          />
+        }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1 )}>
+            More...
+          </Button>
+        </div>
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) =>
-  <form>
-    {children} <input
+const Search = ({ value, onChange, onSubmit, children }) =>
+  <form onSubmit={onSubmit}>
+    <input
       type="text"
       value={value}
       onChange={onChange}
     />
+    <button type="submit">
+      {children}
+    </button>
   </form>
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={largeColumn}>
           <a href={item.url}>{item.title}</a>
